0c4a209a97c05db126deb1ae8f498be1e9cad95f
felix <2070094@cegepmontpetit.ca>
2021-09-29 14:50:35 -0400

    Création du projet

---
+++ app/src/main/java/f/gagne/drapeau/MainActivity.java
@@ -0,0 +1,14 @@
+package f.gagne.drapeau;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
